b9d7b9e1e12048bb346b3831e6f0f1b008fe5ba9
chenlin <chenlin19970710@qq.com>
2019-06-17 17:56:27 +0800

    ??

--- index.js
+++ index.js
@@ -11,9 +11,12 @@ const mongoose = require("mongoose");
 const app = new Koa();
 
 //连接数据库。
-const mongodb = mongoose.connect("mongodb://chenlin:19970520@www.chenyibai.cn:27017/my", {
-  useNewUrlParser: true
-});
+const mongodb = mongoose.connect(
+  "mongodb://chenlin:19970520@www.chenyibai.cn:27017/my",
+  {
+    useNewUrlParser: true
+  }
+);
 
 var userSchema = new mongoose.Schema({
   username: String,
@@ -32,20 +35,20 @@ var newUser = new user(u);
 
 //读取test库中的show集合
 // const user = mongodb.get("tt");
-
 const main = async ctx => {
+  // 配置静态资源
   const data = await user.find({}, { _id: 0 });
   ctx.response.body = data;
 };
-const index = async ctx=>{
+const index = async ctx => {
   ctx.response.type = "html";
-  ctx.response.body = fs.createReadStream("complate/index.html")
-}
-app.use(route.get("/",index));
+  ctx.response.body = fs.createReadStream("complate/index.html");
+};
+console.log(__dirname)
+app.use(route.get("/", index));
+app.use(serve(path.join(__dirname + "/static")));
 
-// 配置静态资源 
-app.use(serve(path.join(__dirname)));
-var server = app.listen(3000, () => {
+var server = app.listen(3333, () => {
   const host = server.address().address;
   const port = server.address().port;
   console.log(`${host},${port}`);
